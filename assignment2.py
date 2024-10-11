28ef906e503ab4e3089c0ef70fad3e85ffceefd2
tahidul942 <mdtahidulislam@unomaha.edu>
2024-10-11 00:35:46 -0500

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,4 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -9,7 +9,7 @@ features = df.drop(["meal", "id", "DateTime"], axis=1)
 
 train_X, test_X, train_Y, test_Y = train_test_split(features, target, test_size=0.33, random_state=42)
 
-model = DecisionTreeClassifier(max_depth=10, min_samples_leaf=10)
+model = RandomForestClassifier(n_estimators=100, max_depth=10, min_samples_leaf=10, random_state=42)
 modelFit = model.fit(train_X, train_Y)
 
 print(f"\n\nIn-sample accuracy: {round(100 * accuracy_score(train_Y, model.predict(train_X)), 2)}%\n\n")
