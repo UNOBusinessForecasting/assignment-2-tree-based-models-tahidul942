b9f7295e138397ca2bf6a72f5571c8a31e46c2a3
tahidul942 <mdtahidulislam@unomaha.edu>
2024-10-11 00:07:31 -0500

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -2,41 +2,21 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
-# Load the dataset
-data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
+data = pd.read_csv( "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv" )
+data.head()
 
-# Define features and target variable
-X = data.drop(columns=["meal", "id", "DateTime"])
-y = data["meal"]
+model = DecisionTreeClassifier( max_depth = 10, min_samples_leaf = 10 )
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+Y, X = data["meal"], data.drop( ["meal", "id", "DateTime"], axis=1 )
+x, xt, y, yt = train_test_split( X, Y, test_size=0.33, random_state=42 )
 
-# Initialize the Decision Tree model
-model = DecisionTreeClassifier(max_depth=8, min_samples_leaf=15, random_state=42)
+modelFit = model.fit( x,y )
 
-# Fit the model on the training data
-modelFit = model.fit(X_train, y_train)
+print( "\n\nIn-sample accuracy: %s%%\n\n" % str(round(100*accuracy_score(y, model.predict(x)), 2)) )
+print( "\n\nOut-of-sample accuracy: %s%%\n\n" %str(round(100*accuracy_score(yt, model.predict(xt)), 2)) )
 
-# Calculate in-sample accuracy (training set)
-in_sample_accuracy = accuracy_score(y_train, model.predict(X_train))
-print(f"In-sample accuracy: {in_sample_accuracy * 100:.2f}%")
-
-# Calculate out-of-sample accuracy (testing set)
-out_of_sample_accuracy = accuracy_score(y_test, model.predict(X_test))
-print(f"Out-of-sample accuracy: {out_of_sample_accuracy * 100:.2f}%")
-
-# Load the test dataset for predictions
-test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
-X_new = test.drop(columns=["meal", "id", "DateTime"])
-
-# Show the first few rows of the new test data
-X_new.head()
-
-# Make predictions on the new data
-predictions = modelFit.predict(X_new)
-
-# Optional: Store predictions in a DataFrame or CSV file
-predictions_df = pd.DataFrame(predictions, columns=["Predicted Meal"])
-predictions_df.to_csv("meal_predictions.csv", index=False)
+test = pd.read_csv( "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv" )
+testNew = test.drop( ["meal", "id", "DateTime"], axis=1 )
+testNew.head()
