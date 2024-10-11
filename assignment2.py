6cdb84744a023969f74b34c2bf786484c8b8b570
tahidul942 <mdtahidulislam@unomaha.edu>
2024-10-11 00:27:00 -0500

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,49 +1,30 @@
-# Import necessary libraries
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-# Load the training dataset
-data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
+df = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 
-# Define target variable (Y) and features (X), dropping unnecessary columns
-Y = data["meal"]
-X = data.drop(["meal", "id", "DateTime"], axis=1)
+target = df["meal"]
+features = df.drop(["meal", "id", "DateTime"], axis=1)
 
-# Split the data into training and testing sets (33% test size)
-x, xt, y, yt = train_test_split(X, Y, test_size=0.33, random_state=42)
+train_X, test_X, train_Y, test_Y = train_test_split(features, target, test_size=0.33, random_state=42)
 
-# Initialize the DecisionTreeClassifier with parameters
-model = DecisionTreeClassifier(max_depth=10, min_samples_leaf=10)
+tree_model = DecisionTreeClassifier(max_depth=10, min_samples_leaf=10)
+fitted_tree_model = tree_model.fit(train_X, train_Y)
 
-# Fit the decision tree model on the training data
-modelFit = model.fit(x, y)
+train_accuracy = accuracy_score(train_Y, fitted_tree_model.predict(train_X))
+print("\n\nIn-sample accuracy: %s%%\n\n" % str(round(100 * train_accuracy, 2)))
 
-# Evaluate in-sample (training data) accuracy
-in_sample_accuracy = accuracy_score(y, model.predict(x))
-print("\n\nIn-sample accuracy: %s%%\n\n" % str(round(100 * in_sample_accuracy, 2)))
+test_accuracy = accuracy_score(test_Y, fitted_tree_model.predict(test_X))
+print("\n\nOut-of-sample accuracy: %s%%\n\n" % str(round(100 * test_accuracy, 2)))
 
-# Evaluate out-of-sample (test data) accuracy
-out_of_sample_accuracy = accuracy_score(yt, model.predict(xt))
-print("\n\nOut-of-sample accuracy: %s%%\n\n" % str(round(100 * out_of_sample_accuracy, 2)))
+test_df = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
+new_test = test_df.drop(["meal", "id", "DateTime"], axis=1)
 
-# Load the test dataset for predictions
-test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
+test_predictions = fitted_tree_model.predict(new_test)
 
-# Drop unnecessary columns from the test dataset
-testNew = test.drop(["meal", "id", "DateTime"], axis=1)
+test_df["predicted_meal"] = test_predictions
+print(test_df[["id", "predicted_meal"]].head())
 
-# Make predictions on the test dataset
-pred = model.predict(testNew)  # Ensure the prediction variable is named 'pred'
-
-# Store predictions (binary 0 or 1) in the test dataset
-test["meal_predictions"] = pred
-
-# Output the first few rows of the test dataset with predictions
-print(test[["id", "meal_predictions"]].head())
-
-# Save the predictions to a CSV file if needed
-test[["id", "meal_predictions"]].to_csv("meal_predictions.csv", index=False)
-
-print("\nPredictions have been saved to 'meal_predictions.csv'.")
+test_df[["id", "predicted_meal"]].to_csv("meal_predictions.csv", index=False)
